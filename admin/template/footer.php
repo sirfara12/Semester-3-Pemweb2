b026add5116b593b839b2c57213a629659265c22
sirfara12 <143992594+sirfara12@users.noreply.github.com>
2024-11-12 18:53:45 +0700

    Add files via upload

---
+++ admin/template/footer.php
@@ -0,0 +1,8 @@
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDIISYN/
+njoSsxfbrkCTnJrzXt+ENP5MOVBxD+16sEG4zoLp" crossorigin="anonymous"></script>
+<script src="assets/custom/dashboard.js"></script>
+</body>
+
+</html>
